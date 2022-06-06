e658b634181a5c00a7afba777636947b109e9b71
hectorplinio <hectorplinio@gmail.com>
2022-06-06 09:06:03 +0200

    Create About Us

---
+++ components/aboutUs/aboutUs.style.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const AboutUsContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 4.5%;
+    padding: 0 20px;
+    background-color: #ebed8b;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    position: fixed;
+    height: 32%;
+    flex-direction: column;
+    width: 100%;
+`;
+export const AboutUsTitle = styled.h1`
+    font-size: 30px;
+    font-weight: bold;
+    padding: 1em;
+`;
+export const AboutUsText = styled.p`
+    font-size: 20px;
+    font-weight: bold;
+    padding: 1em;
+`;
---
+++ components/aboutUs/aboutUs.tsx
@@ -0,0 +1,21 @@
+import { AboutUsContainer, AboutUsText, AboutUsTitle } from "./aboutUs.style";
+const AboutUs = () => {
+    return (
+        <AboutUsContainer>
+            <AboutUsTitle>About Us</AboutUsTitle>
+            <AboutUsText>
+                Greengrocer is engaged in the marketing of fruits and vegetables
+                worldwide. vegetables all over the world. We are specialists in
+                selecting and producing and producing the best products to offer
+                to our customers. customers.
+            </AboutUsText>
+            <AboutUsText>
+                Our professionals have 25 years of experience in the sector.
+                This gives them a broad knowledge of the market, but also of the
+                products. of the products. Their experience and customer
+                satisfaction are our best guarantee. Would you like to meet us?
+            </AboutUsText>
+        </AboutUsContainer>
+    );
+};
+export default AboutUs;
